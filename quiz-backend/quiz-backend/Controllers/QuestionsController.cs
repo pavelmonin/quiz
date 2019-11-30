5c0a7f5518e49e8cde6948b1a2cccf85e822c044
Paolo Montini <paolomontini123@gmail.com>
2019-11-30 14:04:55 +0300

    first commit

---
+++ quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+
+        readonly QuizContext context;
+
+        public QuestionsController(QuizContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Models.Question question)
+        {
+            //context.Questions.Add(new Models.Question() {Text = "text1" });
+
+            var quiz = context.Quiz.SingleOrDefault(q => q.Id == question.QuizId);
+
+            if (quiz is null)
+            {
+                return NotFound();
+            }
+
+            context.Questions.Add(question);
+            await context.SaveChangesAsync();
+            return Ok(question);
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            //return new Models.Question[] {
+            //    new Models.Question() {Text="test21"},
+            //    new Models.Question() {Text="test21"},
+            //    new Models.Question() {Text="test21"},
+            //    new Models.Question() {Text="test21"},         
+            //};
+
+            return context.Questions;
+        }
+
+        // GET api/values
+        [HttpGet("{quizId}")]
+        public IEnumerable<Models.Question> Get([FromRoute] int quizId)
+        {
+            return context.Questions.Where(q => q.QuizId==quizId);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Models.Question question)
+        {
+            //before
+            //var question = await context.Questions.SingleOrDefaultAsync(q => q.Id == id);
+
+            //return Ok(question);
+
+            //comparing id inside body request and id in url
+            if (id != question.Id)
+                return BadRequest();
+
+            context.Entry(question).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+
+            return Ok(question);
+        }
+
+    }
+}
---
+++ quiz-backend/quiz-backend/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace quiz_backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    });
+            });
+
+            //services.AddDbContext<QuizContext>(opt => opt.UseInMemory);
+#pragma warning disable CS0618 // Type or member is obsolete
+            services.AddDbContext<QuizContext>(opt => opt.UseInMemoryDatabase("quiz"));
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            services.AddDbContext<UserDbContext>(opt => opt.UseInMemoryDatabase("user"));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<UserDbContext>();
+
+            //auth token middleware
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is the secret phrase"));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = signingKey,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            app.UseCors("AllowAll");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            
+            app.UseMvc();
+        }
+    }
+}
